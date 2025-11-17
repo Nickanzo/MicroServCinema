f1e09f097295d4e60b248fd4dc0df499c96d6a27
Nickanzo <nicoeanzola@gmail.com>
2025-11-17 12:26:58 -0300

    Estrutura do microserviço de filme

---
+++ services/filmes.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI, HTTPException
+from datetime import datetime
+from pydantic import BaseModel
+from enum import Enum
+from typing import List, Dict
+
+app = FastAPI(title="filmes")
+
+FILMES: Dict[str, dict] = {}
+
+class GeneroFilme(str, Enum):
+    ACAO = "acao"
+    COMEDIA = "comedia"
+    TERROR = "terror"
+    ROMANCE = "romance"
+    SUSPENSE = "suspense"
+    INFANTIL = "infantil"
+    NACIONAL = "nacional"
+
+class NovoFilme(BaseModel):
+    id: str
+    nome: str
+    genero: GeneroFilme
+    data_lancamento: datetime
+    emCartaz: bool
+    
